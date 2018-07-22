daa492b800ee0d7a0b46c72be0a8d64778d15cef
Gunnar Holwerda <gunnarholwerda@gmail.com>
2018-07-22 13:03:35 -0700

    Make up command more resilient

--- src/commands/up.ts
+++ src/commands/up.ts
@@ -1,11 +1,21 @@
 import { exec } from 'mz/child_process';
 import * as signale from 'signale';
 import { migrate } from './migrate';
+import { buildImages } from './build-images';
 
 export async function up() {
     signale.log('Bringing environment up');
-    await migrate();
+    await buildImages();
     await exec(`docker-compose down`);
     await exec(`docker-compose up -d`);
+    let successful = false;
+    while (!successful) {
+        try {
+            await migrate();
+            successful = true;
+        } catch (e) {
+            continue;
+        }
+    }
     signale.success('Environment is running in background');
 }
