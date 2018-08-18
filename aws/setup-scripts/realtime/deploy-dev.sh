61a13f5e178805c6c3e8bc299a4013b0a042777c
Gunnar Holwerda <gunnarholwerda@gmail.com>
2018-08-18 16:43:52 -0700

    Auto generate self-signed ssl cert on ec2 instance

--- aws/setup-scripts/realtime/deploy-dev.sh
+++ aws/setup-scripts/realtime/deploy-dev.sh
@@ -8,4 +8,4 @@ scp -i $KEY_FILE -r ./curveball-realtime.service ec2-user@$INSTANCE_IP:/home/ec2
 ssh -i $KEY_FILE ec2-user@$INSTANCE_IP "sudo cp ~/curveball-realtime.service /lib/systemd/system/curveball-realtime.service"
 ssh -i $KEY_FILE ec2-user@$INSTANCE_IP "sudo systemctl daemon-reload"
 ssh -i $KEY_FILE ec2-user@$INSTANCE_IP "sudo systemctl enable curveball-realtime"
-ssh -i $KEY_FILE ec2-user@$INSTANCE_IP "sudo systemctl start curveball-realtime"
+ssh -i $KEY_FILE ec2-user@$INSTANCE_IP "sudo systemctl restart curveball-realtime"
--- aws/setup-scripts/realtime/realtime-setup.sh
+++ aws/setup-scripts/realtime/realtime-setup.sh
@@ -7,5 +7,8 @@ sudo touch /var/log/curveball-realtime.log
 sudo chown ec2-user /var/log/curveball-realtime.log
 chmod 0644 /var/log/curveball-realtime.log
 cd ~/realtime
-curl -H "Authorization: token f914916b1886cd4793ae2465a2b08ebd99714b89" -L https://api.github.com/repos/GunnarHolwerda/QuizRealtime/tarball | tar -xz --strip=1
+mkdir etc
+openssl req -newkey rsa:2048 -new -nodes -keyout etc/key.pem -out etc/csr.pem -subj "/C=US/ST=Oregon/L=Portland/O=Dis/CN=$(hostname)"
+openssl x509 -req -days 365 -in etc/csr.pem -signkey etc/key.pem -out etc/server.crt -days 15000
+curl -H "Authorization: token f914916b1886cd4793ae2465a2b08ebd99714b89" -L https://api.github.com/repos/GunnarHolwerda/QuizRealtime/tarball/develop | tar -xz --strip=1
 npm install
