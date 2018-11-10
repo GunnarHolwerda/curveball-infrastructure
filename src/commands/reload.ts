c8a7a93dc3a71fced01c2b26d99ce6560e90a45a
Gunnar Holwerda <gunnarholwerda@gmail.com>
2018-11-10 13:15:53 -0800

    Remove db commands and add logs command

--- src/commands/reload.ts
+++ src/commands/reload.ts
@@ -1,9 +1,18 @@
-import { exec } from 'mz/child_process';
+import { exec, spawn } from 'mz/child_process';
+import { fs } from 'mz';
 import * as signale from 'signale';
-import { InfrastructureDir } from '../constants';
+import { InfrastructureDir, QuizDir } from '../constants';
 
 export async function reload() {
     signale.info('Bringing down environment');
+    await exec('kill $(lsof -t -i:3000) || true');
+    const out = fs.openSync('./out.log', 'a');
+    const err = fs.openSync('./out.log', 'a');
+    spawn('npm', ['run', 'start:local'], {
+        cwd: QuizDir,
+        stdio: ['ignore', out, err], // piping stdout and stderr to out.log
+        detached: true
+    }).unref();
     await exec(`cd ${InfrastructureDir} && docker-compose down`);
     signale.info('Bringing environment up');
     await exec(`cd ${InfrastructureDir} && docker-compose up -d`);
--- src/commands/up.ts
+++ src/commands/up.ts
@@ -1,70 +1,13 @@
-import { Command } from 'commander';
-import { exec } from 'mz/child_process';
 import * as signale from 'signale';
-import * as sleep from 'system-sleep';
-import { DbContainerName } from '../constants';
 import { buildImages } from './build-images';
-import { cleanDb } from './clean-db';
-import { migrate } from './migrate';
 import { reload } from './reload';
 
-export async function up(command: Command) {
+export async function up() {
     try {
         await buildImages();
     } catch (e) {
         return;
     }
     await reload();
-    let successful = false;
-    const msToSleep = 2 * 1000;
-    let running = false;
-
-    signale.await('Waiting for containers');
-    while (!running) {
-        try {
-            await exec(`docker exec -t ${DbContainerName} pg_isready -U admin`);
-            running = true;
-        } catch (e) {
-            sleep(msToSleep);
-        }
-    }
-    if (!running) {
-        signale.error('Environment failed to spin up in time');
-        process.exit(1);
-    }
-
-    signale.await('Detecting curveball database');
-    try {
-        sleep(msToSleep);
-        const result = await exec(`docker exec -t ${DbContainerName} psql -U admin -lqt | cut -d \\| -f 1 | grep -w curveball`);
-        if (result[0].toString().trim() !== 'curveball') {
-            throw new Error('Unable to find curveball db');
-        }
-    } catch (e) {
-        signale.warn('Could not find the curveball database, creating clean database');
-        try {
-            await cleanDb();
-        } catch (e) {
-            process.exit(1);
-        }
-    }
-
-    let attempts = 0;
-    while (!successful && attempts <= 15) {
-        try {
-            await migrate(command);
-            const adminUserInsertFile = '/var/log/pg_schema/admin_user.sql';
-            await exec(`docker exec -t ${DbContainerName} psql -U developer curveball -f ${adminUserInsertFile}`);
-            successful = true;
-        } catch (e) {
-            attempts++;
-            sleep(msToSleep);
-            continue;
-        }
-    }
-    if (attempts === 15) {
-        signale.error('Failed to spin up environment in time');
-        return;
-    }
     signale.success('Environment is running in background');
 }
