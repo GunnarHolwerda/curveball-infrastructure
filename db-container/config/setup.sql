932643dd2832e401193846b1e1e1c6a7f9e49523
Gunnar Holwerda <gunnarholwerda@gmail.com>
2019-01-09 19:26:17 -0800

    Generic column added to question_type

--- db-container/config/setup.sql
+++ db-container/config/setup.sql
@@ -1,1 +1,2 @@
-INSERT INTO question_type (title, description) VALUES ('Manual', 'A question with manual point calculation. You will need to select which answers are correct on your own');
+INSERT INTO question_type (title, description, generic) VALUES 
+('Manual', 'A question with manual point calculation. You will need to select which answers are correct on your own', TRUE);
