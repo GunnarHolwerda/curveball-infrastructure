4b35107df7d0ed91bbfc512aa21ab09f454c3038
Gunnar Holwerda <gunnarholwerda@gmail.com>
2019-01-10 18:06:47 -0800

    Remove old quiz directory code

--- src/commands/reload.ts
+++ src/commands/reload.ts
@@ -1,18 +1,9 @@
-import { exec, spawn } from 'mz/child_process';
-import { fs } from 'mz';
+import { exec } from 'mz/child_process';
 import * as signale from 'signale';
-import { InfrastructureDir, QuizDir } from '../constants';
+import { InfrastructureDir } from '../constants';
 
 export async function reload() {
     signale.info('Bringing down environment');
-    await exec('kill $(lsof -t -i:3000) || true');
-    const out = fs.openSync('./out.log', 'a');
-    const err = fs.openSync('./out.log', 'a');
-    spawn('npm', ['run', 'start:local'], {
-        cwd: QuizDir,
-        stdio: ['ignore', out, err], // piping stdout and stderr to out.log
-        detached: true
-    }).unref();
     await exec(`cd ${InfrastructureDir} && docker-compose down`);
     signale.info('Bringing environment up');
     await exec(`cd ${InfrastructureDir} && docker-compose up -d`);
