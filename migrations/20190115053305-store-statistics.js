d7deab773106a5c1fb94311dadad343c7e6247a2
Gunnar Holwerda <gunnarholwerda@gmail.com>
2019-01-14 21:35:57 -0800

    Store statistics on game

---
+++ migrations/20190115053305-store-statistics.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = async function (db) {
+  await db.addColumn('sport_game', 'statistics', { type: 'json' });
+};
+
+exports.down = async function (db) {
+  await db.removeColumn('sport_game', 'statistics');
+};
+
+exports._meta = {
+  "version": 15
+};
