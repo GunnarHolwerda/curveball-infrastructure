0334f6b028862f3baf149c57ef68d0eb2ac9104e
Gunnar Holwerda <gunnarholwerda@gmail.com>
2019-01-14 19:32:48 -0800

    Database updates for scoring
    Generalizes the external id columns to be external_id and parent_external_id to allow for more generic querying. In addition adds indexes on all of those columns for better query performance. Adds the score column to questions_choices to allow for storing of score information.

---
+++ migrations/20190115032216-external-id-rename.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = async function (db) {
+  await db.renameColumn('sport_team', 'id', 'external_id');
+  await db.renameColumn('sport_season', 'id', 'external_id');
+  await db.renameColumn('sport_game', 'id', 'external_id');
+  await db.renameColumn('sport_player', 'id', 'external_id');
+
+  await db.renameColumn('sport_team', 'season', 'parent_external_id');
+  await db.renameColumn('sport_game', 'season', 'parent_external_id');
+  await db.renameColumn('sport_player', 'team', 'parent_external_id');
+};
+
+exports.down = async function (db) {
+  await db.renameColumn('sport_team', 'external_id', 'id');
+  await db.renameColumn('sport_season', 'external_id', 'id');
+  await db.renameColumn('sport_game', 'external_id', 'id');
+  await db.renameColumn('sport_player', 'external_id', 'id');
+
+  await db.renameColumn('sport_team', 'parent_external_id', 'season');
+  await db.renameColumn('sport_game', 'parent_external_id', 'season');
+  await db.renameColumn('sport_player', 'parent_external_id', 'team');
+};
+
+exports._meta = {
+  "version": 13
+};
---
+++ migrations/20190115032645-choice-score.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = async function (db) {
+  await db.addIndex('sport_season', 'sport_season_external_id_idx', ['external_id']);
+  await db.addIndex('sport_game', 'sport_game_external_id_idx', ['external_id']);
+  await db.addIndex('sport_player', 'sport_player_external_id_idx', ['external_id']);
+  await db.addIndex('sport_team', 'sport_team_external_id_idx', ['external_id']);
+
+  await db.addIndex('sport_game', 'sport_game_parent_external_id_idx', ['parent_external_id']);
+  await db.addIndex('sport_player', 'sport_player_parent_external_id_idx', ['parent_external_id']);
+  await db.addIndex('sport_team', 'sport_team_parent_external_id_idx', ['parent_external_id']);
+
+  await db.addColumn('questions_choices', 'score', { type: 'decimal' });
+  await db.addIndex('questions_choices', 'choices_subject_idx', 'subject_id');
+};
+
+exports.down = async function (db) {
+  await db.removeIndex('sport_season', 'sport_season_external_id_idx');
+  await db.removeIndex('sport_game', 'sport_game_external_id_idx');
+  await db.removeIndex('sport_player', 'sport_player_external_id_idx');
+  await db.removeIndex('sport_team', 'sport_team_external_id_idx');
+  await db.removeIndex('sport_game', 'sport_game_parent_external_id_idx');
+  await db.removeIndex('sport_player', 'sport_player_parent_external_id_idx');
+  await db.removeIndex('sport_team', 'sport_team_parent_external_id_idx');
+
+  await db.removeColumn('questions_choices', 'score');
+  await db.removeIndex('questions_choices', 'choices_subject_idx');
+};
+
+exports._meta = {
+  "version": 14
+};
